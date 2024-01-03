dd0135585ac9755006cd9419ebaf1c657388f161
Rocío Domínguez <rociodominguezcontacto@gmail.com>
2024-01-03 12:22:46 +0100

    v4

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-import './style.css';
-import printNavBar from './src/components/Navbar/Navbar';
+import initializeHangman from './src/components/Hangman/Hangman';
+import initializeRock from './src/components/Rock/Rock';
+import initializeTrivial from './src/components/Trivial/Trivial';
 
-app.appendChild(printNavBar());
+const gameContainer = document.getElementById('app');
+const hangmanLink = document.getElementById('hangman-link');
+const rockLink = document.getElementById('rock-link');
+const trivialLink = document.getElementById('trivial-link');
+
+hangmanLink.addEventListener('click', () => {
+  gameContainer.innerHTML = '';
+  initializeHangman(gameContainer);
+});
+
+rockLink.addEventListener('click', () => {
+  gameContainer.innerHTML = '';
+  initializeRock(gameContainer);
+});
+
+trivialLink.addEventListener('click', () => {
+  gameContainer.innerHTML = '';
+  initializeTrivial(gameContainer);
+});
